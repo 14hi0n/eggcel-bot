771739bf81921504a9ee798130560d2b53747606
Octoober <kirill.job2019@gmail.com>
2026-09-03 03:03:32 +0700

    refactor: check message

--- handlers/start.py
+++ handlers/start.py
@@ -3,8 +3,13 @@ from telegram.ext import ContextTypes
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message = update.effective_message
+
+    if message is None:
+        return
+
     text = (
         "Отправь мне картинку с описанием чтобы сделать из нее мем.\n\n"
         "А еще можешь добавь меня в чат, чтобы я делал мемы из рандомных картинок."
     )
-    await update.message.reply_text(text)
+    await message.reply_text(text)
